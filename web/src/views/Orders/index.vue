80ec21b6182075b8dbb262be1f7daa520ae0fdda
Adam <adam.keristoper@gmail.com>
2020-07-04 10:39:34 +0900

    Move link click region to list item

--- web/src/views/Orders/index.vue
+++ web/src/views/Orders/index.vue
@@ -3,10 +3,12 @@
     <p v-if="!orders.length">There are no orders</p>
     <!-- TODO: extract this UL into it's own component -->
     <ul class="orders" v-else>
-      <li v-for="order in orders" :key="order.id">
-        <router-link :to="{ name: 'OrdersShow', params: { orderId: order.id.toString() }}">
-          Order no. {{ order.id }}
-        </router-link>
+      <li
+        v-for="order in orders"
+        :key="order.id"
+        @click="onOrderListClick(order.id)"
+      >
+        <span>Order no. {{ order.id }}</span>
       </li>
     </ul>
   </div>
@@ -19,6 +21,12 @@ export default {
   data () {
     return {
       orders: []
+    }
+  },
+
+  methods: {
+    onOrderListClick (orderId) {
+      this.$router.push({ name: 'OrdersShow', params: { orderId: orderId.toString() }})
     }
   },
 
