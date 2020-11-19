8fc51888af534468689c21691a8e31a5f9efff31
Adam Christopher <adam.keristoper@gmail.com>
2020-11-19 11:58:17 +0900

    Add versioning to service worker
    This hopefully invalidates cache of the service worker

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   pwa: {
     name: 'Kristalian System',
-    themeColor: '#364021'
+    themeColor: '#364021',
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'service-worker.js',
+    },
   },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/'
