6c8f8d6220f3cb31b6a7bfd9afa38f9f17c55938
Adam Christopher <adam.keristoper@gmail.com>
2021-01-13 12:18:54 +0900

    Revert back to GenerateSW

--- web/vue.config.js
+++ web/vue.config.js
@@ -2,10 +2,7 @@ module.exports = {
   pwa: {
     name: 'Kristalian System',
     themeColor: '#364021',
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      swSrc: 'src/service-worker.js',
-    },
+    workboxPluginMode: 'GenerateSW',
   },
   publicPath: process.env.NODE_ENV === 'production'
     ? '/'
